2c941ac13a56126c040753bcc36479355ffbee09
Modassir Ali <alimodassir@gmail.com>
2021-10-27 14:52:58 +0530

    added contact controller

--- routes/feedbackRoute.js
+++ routes/feedbackRoute.js
@@ -3,5 +3,6 @@ const feedbackController = require("../controllers/feedbackController");
 const router = Router();
 
 router.post("/getFeedback", feedbackController.getFeedBack);
+router.post("/contact", feedbackController.contact);
 
 module.exports = router;
